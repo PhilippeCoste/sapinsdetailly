c74c4e63b4873cd5646feab0c003d29a73a00502
Philippe Coste <philippecostelh@gmail.com>
2017-08-08 16:18:19 +0200

    Routes created

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def accueil
+  end
+
+  def produits
+  end
+
+  def fabrication
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/accueil'
+
+  get 'pages/produits'
+
+  get 'pages/fabrication'
+
+  get 'pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
